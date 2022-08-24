08613ac186cb00459f7083908376317bec81d1ce
Kira <starkial@lenta.ru>
2022-08-24 16:11:00 +0500

    Task 37 среднее число без пары выводим само

---
+++ Task_37/Program.cs
@@ -0,0 +1,46 @@
+﻿// Одномерный массив. Найти произведения чисел:
+// первый*последний, второй*предпоследний и тд
+//результат в новый массив
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for(int i = 0; i<length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int len = array.Length;
+    for(int i = 0; i < len; i++)
+        Console.Write($"{array[i]} ");
+
+    Console.WriteLine();
+}
+
+int[] CreateNewArray(int[] array)
+{
+    int len = array.Length;
+    int new_len = 0;
+
+    if (len % 2 == 0) new_len = len / 2;
+    else new_len = len / 2 + 1;
+
+    int[] new_array = new int[new_len];
+
+    for(int i = 0; i < new_len; i++)
+    {
+        new_array[i] = array[i] * array[len - 1 - i];
+    }
+    if(len % 2 != 0) new_array[len / 2] = array[len / 2];
+    return new_array;
+}
+
+int[] array = CreateArray(5, 1, 9);
+PrintArray(array);
+int[] new_array = CreateNewArray(array);
+PrintArray(new_array);
+               
--- Task_38/Program.cs
+++ Task_38/Program.cs
@@ -1,7 +1,7 @@
 ﻿//массив вещественных чисел (5). разницу между максимальным и минимальным элементов массива
 
 
-double[] CreateArray(int length, int min, int max)
+double[] CreateArray(int length, int min, int max) // формируем массив вещ. чисел
 {
     double[] array = new double [length];
     for(int i = 0; i<length; i++)
@@ -11,7 +11,7 @@ double[] CreateArray(int length, int min, int max)
     return array;
 }
 
-void PrintArray(double[] array)
+void PrintArray(double[] array) //вывод массива
 {
     for(int i = 0; i<array.Length; i++)
        Console.Write($"{array[i]} ");
@@ -19,7 +19,7 @@ void PrintArray(double[] array)
        Console.WriteLine();
 }
 
-double FindDiff(double[] array)
+double FindDiff(double[] array) // ищем разницу м/у max и min
 {
     double min = array[0];
     double max = array[0];
